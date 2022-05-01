df58b9138aab67363a9897432b78b59da5bafcfe
prathima <prathima.reddy.jc@gmail.com>
2022-05-01 16:28:45 +0530

    Initial Commit

---
+++ practiceTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceTableView
+//
+//  Created by Prathima Juturu Chinna on 01/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
